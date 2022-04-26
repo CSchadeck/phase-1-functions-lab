18b9c333fe29d60c733f69477438f07ac7a9dec7
Corey Schadeck <corey@schadeck.com>
2022-04-26 11:33:25 -0400

    commit tree b1fcb8387895b8e2aac647ece52ffe63e0a7c14c

--- index.js
+++ index.js
@@ -10,31 +10,28 @@ function distanceFromHqInBlocks(someValue) {
 
 function distanceFromHqInFeet(someValue) {
     const blocks = distanceFromHqInBlocks(someValue);
-    // call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
-    // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
     return blocks * 264;
-    // the return value of distanceFromHqInBlocks can then be used to calculate fee
+
 }
 
 function distanceTravelledInFeet(start, destination) {
-    //returns the number of feet traveled
     const traveled = start - destination;
     const toFeet = traveled * 264;
     if (toFeet < 0) {
-        return toFeet * -1; 
+        return toFeet * -1;
     } else {
         return toFeet;
     }
 }
 
 function calculatesFarePrice(start, destination) {
-    //returns the fare for the customer
+
     const distance = distanceTravelledInFeet(start, destination);
     if (distance < 401) {
         return 0;
     } else if (distance > 400 && distance < 2001) {
         return (distance - 400) * 2 / 100;
-    } else if (distance > 2000 && distance < 2500){
+    } else if (distance > 2000 && distance < 2500) {
         return 25;
     } else {
         return 'cannot travel that far';
